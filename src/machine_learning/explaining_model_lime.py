5a4d1aecc4e25f5111bd3f09f1de8deb3744b728
CowKeyMan <dancauchi1@gmail.com>
2022-03-14 19:17:52 +0200

    Improve model explanation code

--- src/machine_learning/explaining_model_lime.py
+++ src/machine_learning/explaining_model_lime.py
@@ -7,52 +7,22 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from machine_learning.utils import extract_target_feature
 
-columns_to_drop = [
-    'ShyBirdsPercentage_Nearby_200',
-    'NestID',
-    'Year',
-    'Laydate_first_egg',
-    'Date_trial',
-    'Days_from_LD',
-    'Rasps',
-    'Bill_snaps',
-    'SnapsRasps',
-    'Site',
-    'lat',
-    'long',
-    'Date_nest_found',
-    'New_rebuild',
-    'Rebuild_original',
-    'Shape',
-    'Nearby',
-    'Water_area',
-    'ClusterID_15',
-    'ClusterID_30',
-    'ClusterID_50',
-    'ClusterID_100',
-    'ClusterID_200',
-    'ClusterID_300',
-    # 'Propensity',
-    'Propensity_0',
-    'Propensity_17.5',
-    'Propensity_19.5',
-]
-
 model: sklearn.tree.DecisionTreeClassifier = joblib.load(
     'resources/machine_learning_results/models/BalancedDecisionTree'
 )
 
+target = 'Propensity'
 train = (
     pd.read_csv('resources/generated_data/nest_features_train.csv')
-    .drop(columns=columns_to_drop)
+    [list(model.feature_names_in_) + [target]]
 )
 test = (
-    pd.read_csv('resources/generated_data/nest_features_test.csv').
-    drop(columns=columns_to_drop)
+    pd.read_csv('resources/generated_data/nest_features_test.csv')
+    [list(model.feature_names_in_) + [target]]
 )
 
-train_x, train_y = extract_target_feature(train, 'Propensity')
-test_x, test_y = extract_target_feature(test, 'Propensity')
+train_x, train_y = extract_target_feature(train, target)
+test_x, test_y = extract_target_feature(test, target)
 
 explainer = lime.lime_tabular.LimeTabularExplainer(
     train_x.values,
--- src/machine_learning/explaining_models.py
+++ src/machine_learning/explaining_models.py
@@ -9,7 +9,7 @@ from sklearn.inspection import permutation_importance
 from machine_learning.utils import extract_target_feature
 
 model: sklearn.tree.DecisionTreeClassifier = joblib.load(
-    'resources/machine_learning_results/models/DecisionTree'
+    'resources/machine_learning_results/models/BalancedDecisionTree'
 )
 
 forest_importances = pd.Series(
@@ -23,51 +23,18 @@ forest_importances.plot.bar(yerr=std, ax=ax)
 ax.set_title("Feature importances using Mean Decrease Impurity (MDI)")
 ax.set_ylabel("Mean decrease in impurity")
 fig.tight_layout()
-plt.show()
+plt.show(block=False)
 
 
-columns_to_drop = [
-    'NestID',
-    'Year',
-    'Laydate_first_egg',
-    'Date_trial',
-    'Days_from_LD',
-    'Rasps',
-    'Bill_snaps',
-    'SnapsRasps',
-    'Site',
-    'lat',
-    'long',
-    'Date_nest_found',
-    'New_rebuild',
-    'Rebuild_original',
-    'Shape',
-    'Nearby',
-    'Water_area',
-    'ClusterID_15',
-    'ClusterID_30',
-    'ClusterID_50',
-    'ClusterID_100',
-    'ClusterID_200',
-    'ClusterID_300',
-    # 'Propensity',
-    'Propensity_0',
-    'Propensity_17.5',
-    'Propensity_19.5',
-]
-
+target = 'Propensity'
 test = (
-    pd.read_csv('resources/generated_data/nest_features_test.csv').
-    drop(columns=columns_to_drop)
+    pd.read_csv('resources/generated_data/nest_features_test.csv')
+    [list(model.feature_names_in_) + [target]]
 )
-
-test_x, test_y = extract_target_feature(test, 'Propensity')
-
-
+test_x, test_y = extract_target_feature(test, target)
 result = permutation_importance(
     model, test_x, test_y, n_repeats=10, random_state=42, n_jobs=2
 )
-
 forest_importances = pd.Series(
     result.importances_mean, index=model.feature_names_in_
 )
