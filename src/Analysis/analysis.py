881592d4876e9dfb5f71e934a9b23c8f89246c42
krn-hov <hkaren97@gmail.com>
2022-03-14 17:27:23 +0200

    Add files via upload

---
+++ src/Analysis/analysis.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from autocorrelation import join_counts,random_join_counts
+
+df = pd.read_csv('resources/generated_data/joined_dataset.csv', index_col = False)
+
+#seperating years
+df19 = df[df.Year == 2019]
+df20 = df[df.Year == 2020]
+df21 = df[df.Year == 2021]
+
+#setting index to be observation number
+df19.index = range(df19.shape[0])
+df20.index = range(df20.shape[0])
+df21.index = range(df21.shape[0])
+
+# Real Join Counts
+JC19 = join_counts(df19,15)
+JC20 = join_counts(df20,15)
+JC21 = join_counts(df21,15)
+print("2019 Join Counts: ",JC19,"\n2020 Join Counts: ",JC20,"\n2021 Join Counts: ",JC21)
+
+# Randomized Join Counts
+rand_JC19 = random_join_counts(df19,dis_threshold = 15,repetitions = 500)
+rand_JC20 = random_join_counts(df20,15,500)
+rand_JC21 = random_join_counts(df21,15,500)
+
+def make_hist(list,bins,line_value,x_label):
+    """ makes histogram with the x label being the number of join-counts in a simulation of certain type
+        and the y label being the occurences across all simulations. plots the real value as a line
+        so we can compare it against a random distribution"""
+    plt.hist(list,bins = bins,ec = 'black')
+    plt.ylabel('Counts')
+    plt.xlabel(x_label)
+    plt.axvline(x=line_value, color='r', linestyle='dashed', linewidth=2)
+    #plt.show()
+
+fig=plt.figure()
+
+ax = fig.add_subplot(3,3,1)
+make_hist(rand_JC19['AA'],bins = 10,line_value = JC19['AA'],x_label = 'AA 19')
+ax = fig.add_subplot(3,3,2)
+make_hist(rand_JC19['SS'],bins = 10,line_value = JC19['SS'],x_label = 'SS 19')
+ax = fig.add_subplot(3,3,3)
+make_hist(rand_JC19['AS'],bins = 10,line_value = JC19['AS'],x_label = 'AS 19')
+
+ax = fig.add_subplot(3,3,4)
+make_hist(rand_JC20['AA'],bins = 10,line_value = JC20['AA'],x_label = 'AA 20')
+ax = fig.add_subplot(3,3,5)
+make_hist(rand_JC20['SS'],bins = 10,line_value = JC20['SS'],x_label = 'SS 20')
+ax = fig.add_subplot(3,3,6)
+make_hist(rand_JC20['AS'],bins = 10,line_value = JC20['AS'],x_label = 'AS 20')
+
+ax = fig.add_subplot(3,3,7)
+make_hist(rand_JC21['AA'],bins = 10,line_value = JC21['AA'],x_label = 'AA 21')
+ax = fig.add_subplot(3,3,8)
+make_hist(rand_JC21['SS'],bins = 10,line_value = JC21['SS'],x_label = 'SS 21')
+ax = fig.add_subplot(3,3,9)
+make_hist(rand_JC21['AS'],bins = 10,line_value = JC21['AS'],x_label = 'AS 21')
+
+fig.tight_layout()
+plt.show()
+
+
+
+# print()
+
+# print("randomized join counts")
+# print("2019: ",random_join_counts(df19,50,5))
+# print("2020: ",random_join_counts(df20,50,5))
+# print("2021: ",random_join_counts(df21,50,5))
---
+++ src/Analysis/autocorrelation.py
@@ -0,0 +1,156 @@
+from tabnanny import check
+import pandas as pd
+import numpy as np
+import geopy.distance
+
+
+df = pd.read_csv('resources/generated_data/joined_dataset.csv', index_col = False)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+#dis_threshold is in m
+#takes in array of distances b/w observations
+def join_counts(df,dis_threshold):
+
+    obsv = df.shape[0] #observations  
+
+    neighbor_mtx = create_neighbor_matrix(df,dis_threshold) #matrix indicating which observations are neighbors with each other
+    
+    # aggressive-aggressive neighbors, shy-shy, aggressive-shy
+    AA,SS,AS = 0,0,0 
+
+    values = df['Propensity'].to_numpy().reshape(obsv,1)
+
+    #filling in the join counts
+    for i in range(obsv):
+        for j in range(obsv):
+            if neighbor_mtx[i,j] == 1:
+                if values[i] == 1 and values[j] == 1:
+                    AA += 1
+                elif values[i] == 0 and values[j] == 0:
+                    SS += 1
+                else:
+                    AS += 1
+
+    #halving because of matrix symmetry
+    AA = AA * 0.5
+    SS = SS * 0.5
+    AS = AS * 0.5
+
+    return {'AA':AA,'SS':SS,'AS':AS}
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def random_join_counts(df, dis_threshold, repetitions = 1) -> dict:
+
+    obsv = df.shape[0] #observations 
+
+    neighbor_mtx = create_neighbor_matrix(df, dis_threshold)
+
+    one_counts = int(neighbor_mtx.sum() / 2)
+
+    col_count = int(obsv * (obsv - 1) / 2)
+
+    vector_zeros = np.zeros((1,col_count - one_counts))
+
+    vector_ones = np.ones((1,one_counts))
+
+    random_order = np.concatenate((vector_ones,vector_zeros),axis=None)
+
+    values = df['Propensity'].to_numpy().reshape(obsv,1)
+
+    rand_neighbor_mtx = np.zeros((obsv,obsv))
+
+    reps = 0
+
+    results = {'AA':[],'SS':[],'AS':[]}
+
+    while(reps < repetitions):
+
+        np.random.shuffle(random_order)
+
+        ind = 0
+
+        for i in range(obsv):
+            for j in range(obsv):
+                if i==j:
+                    rand_neighbor_mtx[i,j] = 0
+                elif i > j:
+                    rand_neighbor_mtx[i,j] = rand_neighbor_mtx[j,i]
+                else:
+                    rand_neighbor_mtx[i,j] = random_order[ind]
+                    ind += 1
+        
+        AA,SS,AS = 0,0,0
+
+        for i in range(obsv):
+            for j in range(obsv):
+                if rand_neighbor_mtx[i,j] == 1:
+                    if values[i] == 1 and values[j] == 1:
+                        AA += 1
+                    elif values[i] == 0 and values[j] == 0:
+                        SS += 1
+                    else:
+                        AS += 1
+
+        #halving because of matrix symmetry
+        AA = AA * 0.5
+        SS = SS * 0.5
+        AS = AS * 0.5
+
+        results['AA'].append(AA)
+        results['SS'].append(SS)
+        results['AS'].append(AS)
+
+        reps += 1
+
+    return results
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def create_neighbor_matrix(df,dis_threshold):
+
+    obsv = df.shape[0] #observations  
+
+    neighbor_mtx = np.zeros((obsv,obsv)) #matrix indicating which observations are neighbors with each other
+    
+    dm = create_distance_matrix(df)
+
+    #filling in the neighbor matrices
+    for i in range(obsv):
+        for j in range(obsv):
+            if i==j:
+                continue
+            if (dm[i,j] <= dis_threshold):
+                neighbor_mtx[i,j] = 1
+    
+    return neighbor_mtx
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+#create matrix of distances between observations
+def create_distance_matrix(df):
+
+    #setting index to be observation number
+    df.index = range(df.shape[0])
+
+    obsv = df.shape[0] #observations  
+
+    dist_mtx = np.zeros((obsv,obsv)) #matrix indicating which observations are neighbors with each other
+    
+    #filling in the neighbor matrices
+    for index,row in df.iterrows():
+        lat,long = row['lat'],row['long']
+        for index2,row2 in df.iterrows():
+            lat2,long2 = row2['lat'],row2['long']
+            coords1 = (lat,long)
+            coords2 = (lat2,long2)
+            dist_mtx[index,index2] = geopy.distance.distance(coords1,coords2).km*1000
+    
+    return dist_mtx
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def check_symmetric(a, rtol=1e-05, atol=1e-08):
+    return np.allclose(a, a.T, rtol=rtol, atol=atol)
+    
