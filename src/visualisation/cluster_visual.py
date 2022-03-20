aee04378c39b84c8a8a0287d05c595e5422fb342
outisa <savolainen.outi@gmail.com>
2022-03-20 14:19:12 +0200

    Create maps with mean centers for diff. types of nests

--- src/visualisation/cluster_visual.py
+++ src/visualisation/cluster_visual.py
@@ -6,23 +6,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-def fetch_data():
-    df_general_data = pd.read_csv(
-    'resources/generated_data/joined_dataset.csv', index_col='NestID'
-    )
-    df_clusters = pd.read_csv(
-    'resources/generated_data/clusters.csv', index_col='NestID'
-    )
-    df_location_data = pd.read_csv( 
-    'resources/original_data/FinlandNestDatafile.csv', index_col='NestID'
-    )
-    df_location_data21 = pd.read_csv( 
-    'resources/original_data/Finland_nestdata2021_mod.csv', index_col='NestID'
-    )
-    df_location_data21['Year'] = 2021
-    df_location_data = pd.concat([df_location_data, df_location_data21])
-    return df_general_data, df_clusters, df_location_data
+from utils.data_utils import fetch_data
 
 def generate_cluster_info(cluster: int, year: int, general_data: pd.DataFrame, 
     nests: pd.DataFrame, cluster_size_data: pd.DataFrame, dist:int):
