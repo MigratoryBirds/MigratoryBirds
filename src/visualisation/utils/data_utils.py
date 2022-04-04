eb0c04550c5a2b8732c71725b8d6c0deb8a322ca
outisa <savolainen.outi@gmail.com>
2022-04-04 20:26:20 +0300

    Add site data plots

--- src/visualisation/utils/data_utils.py
+++ src/visualisation/utils/data_utils.py
@@ -1,17 +1,21 @@
+from numpy import add
 import pandas as pd
 
-def fetch_data():
+def fetch_data(dots=True):
+    add_dots = ''
+    if dots:
+        add_dots ='../../'
     df_general_data = pd.read_csv(
-    'resources/generated_data/joined_dataset.csv', index_col='NestID'
+    f'{add_dots}resources/generated_data/joined_dataset.csv', index_col='NestID'
     )
     df_clusters = pd.read_csv(
-    'resources/generated_data/clusters.csv', index_col='NestID'
+    f'{add_dots}resources/generated_data/clusters.csv', index_col='NestID'
     )
     df_location_data = pd.read_csv( 
-    'resources/original_data/FinlandNestDatafile.csv', index_col='NestID'
+    f'{add_dots}resources/original_data/FinlandNestDatafile.csv', index_col='NestID'
     )
     df_location_data21 = pd.read_csv( 
-    'resources/original_data/Finland_nestdata2021_mod.csv', index_col='NestID'
+    f'{add_dots}resources/original_data/Finland_nestdata2021_mod.csv', index_col='NestID'
     )
     df_location_data21['Year'] = 2021
     df_location_data = pd.concat([df_location_data, df_location_data21])
--- src/visualisation/yearly_clustermaps.py
+++ src/visualisation/yearly_clustermaps.py
@@ -4,23 +4,11 @@
 """
 import folium
 from folium.plugins import MarkerCluster
-import pandas as pd
 import numpy as np
 
-df_general_data = pd.read_csv(
-  'resources/generated_data/joined_dataset.csv', index_col='NestID'
-)
-df_clusters = pd.read_csv(
-  'resources/generated_data/clusters.csv', index_col='NestID'
-)
-df_location_data = pd.read_csv( 
-  'resources/original_data/FinlandNestDatafile.csv', index_col='NestID'
-)
-df_location_data21 = pd.read_csv( 
-  'resources/original_data/Finland_nestdata2021_mod.csv', index_col='NestID'
-)
-df_location_data21['Year'] = 2021
-df_location_data = pd.concat([df_location_data, df_location_data21])
+from utils.data_utils import fetch_data
+
+df_general_data, df_clusters, df_location_data = fetch_data()
 
 YEARS = df_clusters['Year'].unique()
 # Basic cluster map with folium
